6552ec643932181c56827e49c52f1b6781ceb8b0
Patrick <patrickcalleja@protonmail.com>
2019-10-22 22:37:18 -0400

    autofilled saved pt. Added select form

--- chrome/options.js
+++ chrome/options.js
@@ -1,42 +1,45 @@
-// let page = document.getElementById('buttonDiv');
-// const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-// function constructOptions(kButtonColors) {
-//   for (let item of kButtonColors) {
-//     let button = document.createElement('button');
-//     button.style.backgroundColor = item;
-//     button.addEventListener('click', function() {
-//       chrome.storage.sync.set({color: item}, function() {
-//         console.log('color is ' + item);
-//       })
-//     });
-//     page.appendChild(button);
-//   }
-// }
-// constructOptions(kButtonColors);
-
 let pt;
 
 let pt_input = document.getElementById('pt');
 let submit = document.getElementById('submit');
 
+function setSubmitTextField(pt_value) { 
+  pt_input.value = pt_value;
+}
 
-chrome.storage.local.get(['pt'], function(result) {
-  if (chrome.runtime.lastError){
+chrome.storage.local.get(['pt'], (result) => {
+  if (chrome.runtime.lastError) {
     return;
   }
-  console.log('Value is set to ' + result.pt);
-  setSubmitTextField(result.pt, submit);
+  setSubmitTextField(result.pt);
 });
 
 submit.onclick = () => {
   pt_value = pt_input.value;
   chrome.storage.local.clear();
-  chrome.storage.local.set({pt: pt_value}, function() {
+  chrome.storage.local.set({pt: pt_value}, () => {
     console.log('Value is set to ' + pt_value);
   });
+}
+
+
+
+// Getting data
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    parseJson(this.responseText);
+  }
+});
+xhr.open("GET", "https://api.youneedabudget.com/v1/budgets");
+xhr.setRequestHeader("Authorization", "Bearer a7210a4dd1e40b3c52e1e11b8f877c6bb9afe0520a7f42c82f174b0b7d475bba");
+xhr.send(null);
+
+function parseJson(data) {
+  var budgets = JSON.parse(data)["data"]["budgets"];
+  
 };
 
-function setSubmitTextField(pt_value, submit_input) { 
-  console.log(pt_value, submit_input);
-  pt_input.value = pt_value;
-};
+
