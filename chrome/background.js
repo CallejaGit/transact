285f4f848eee71e14df0c70fb362597ad155084f
Patrick <patrickcalleja@protonmail.com>
2019-11-04 18:20:11 -0500

    test button on bank page

--- chrome/background.js
+++ chrome/background.js
@@ -1,3 +1,1 @@
-  chrome.webNavigation.onCompleted.addListener(function() {
-      alert("This is my favorite website!");
-  }, {url: [{urlMatches : 'https://www.google.com/'}]});
+console.log("HELLO")
