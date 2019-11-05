ec8f76cee85d107baca7e1e4f0639c546c3c6213
Patrick <patrickcalleja@protonmail.com>
2019-11-04 20:16:49 -0500

    set up TODOs and HTML Object for parsing

---
+++ chrome/content.js
@@ -0,0 +1,18 @@
+// Checking page title
+
+console.log("TEST");
+
+if (document.title.indexOf("EasyWeb") != -1) {
+
+  var x = document.getElementsByTagName("trable")[3];
+}
+
+/**
+ * TODO: ----Overview Objectives----
+ * Replace Dates with input as the current date
+ * Replace Transaction Descriptions to input form
+ * Same with Debit and Balance
+ * Add a checkmark button
+ * Once Button is set it will 
+ * */
+
