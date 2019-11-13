84f05fed4ac7a473e3c3b06a45e0740a5a4b0467
Patrick <patrickcalleja@protonmail.com>
2019-11-12 22:02:02 -0500

    switch to SemanticUI

--- chrome/content.js
+++ chrome/content.js
@@ -12,11 +12,13 @@
  */
 setTimeout(function(){
   main();
-}, 1000);
+}, 2000);
 
-function treadRowData(transactionsTable) {
+function treadRowData(id, transactionsTable) {
+
 
   var tableElem = transactionsTable.getElementsByTagName('td');
+
 
   var data = { 
     date: tableElem[0].textContent.split(/\r?\n/)[5].trim(),
@@ -24,114 +26,140 @@ function treadRowData(transactionsTable) {
     debit: tableElem[2].textContent.split(/\r?\n/)[2].trim(),
     credit: tableElem[3].textContent.split(/\r?\n/)[0].trim(),
     balance: tableElem[4].textContent.split(/\r?\n/)[0].trim(),
+    id: id 
   };
   return data;
-  /**
-  for (var i = 0, len = tableElem.length; i < len; i++) {
-    if (data[i]){
-      var input = document.createElement("INPUT"); 
-      input.setAttribute("type", "text");
-      input.value = data[i];
-      tableElem[i].appendChild(input);
-    }
-  }
-  /** 
-   * NTS: 
-   * use datalist to populate this list
-   * add a button
-   * select category
-   * 
-   */
-
 }
 
-function initBootstrap() {
+function initSemanticUI() {
 
-  var head = document.head;
-  var link = document.createElement('link');
+  $(document.head).append(`
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+  `);
 
-  var script = document.createElement('script');
-  var script2 = document.createElement('script');
-  var script3 = document.createElement('script');
-
-  // CSS
-  link.rel = "stylesheet";
-  link.href = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css";
-  link.setAttribute("integrity", "sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T");
-  link.crossOrigin = "anonymous";
-  document.head.appendChild(link);
-
-  // JS
-  script.src = "https://code.jquery.com/jquery-3.3.1.slim.min.js";
-  script.setAttribute("integrity", "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo");
-  script.crossOrigin = "anonymous";
-  document.body.appendChild(script);
-  script2.src = "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js";
-  script2.setAttribute("integrity", "sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1");
-  script2.crossOrigin = "anonymous";
-  document.body.appendChild(script2);
-  script3.rel = "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js";
-  script3.setAttribute("integrity", "sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM");
-  script3.crossOrigin = "anonymous";
-  document.body.appendChild(script3);
 }
 
 function insertTable() {
-  $("#transactionsTable").replaceWith("\
-    <table id='ynabTable' class='table table-striped'>\
-  <thead>\
-    <tr>\
-      <th scope='col'>DATE</th>\
-      <th scope='col'>PAYEE</th>\
-      <th scope='col'>CATEGORY</th>\
-      <th scope='col'>MEMO</th>\
-      <th scope='col'>OUTFLOW</th>\
-      <th scope='col'>INFLOW</th>\
-      <th scope='col'>CONFIRM</th>\
-    </tr>\
-  </thead>\
-  <tbody>\
-  </tbody>\
-</table>\
-    "); 
-
+  $("#transactionsTable").replaceWith(`
+    <table id="ynabTable" class="ui blue table">
+      <thread>
+        <tr>
+          <th>DATE</th>
+          <th>PAYEE</th>
+          <th>CATEGORY</th>
+          <th>MEMO</th>
+          <th>OUTFLOW</th>
+          <th>INFLOW</th>
+          <th></th>
+        </tr>
+      </thread>
+      <tbody>
+      </tbody>
+        <tfoot class="full-width">
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+              <th colspan="6">
+              <div class="ui small blue button">
+                Approve
+              </div>
+              <div class="ui small blue disabled button">
+                Approve All
+              </div>
+            </th>
+          </tr>
+        </tfoot>
+    </table>
+  
+  `);
   return $("#ynabTable");
 }
 
 function fillTable(ynabTable, transactions) {
 
-  console.log(transactions);
-  var ynabBody = ynabTable.find('tbody');
-  
+  var ynabBody = ynabTable.find('tbody').first();
+ 
 
+  /**
+   * TODO: Bug when setting payee_name to the placeholder
+   *
+   * It takes the first word of the string as the value.
+   */
   transactions.forEach((arrItem) => {
     ynabBody.append(`
-    <tr>
-      <td>${arrItem.date}</th>
-      <td>${arrItem.payee_name}</td>
-      <td>empty</td>
+    <tr id=${'ynabTrans' + arrItem.id}>
+      <td style="padding:10px"; style="margin:0px" id="date"></td>
       <td>
-        <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+        <div class="ui input">
+          <input type="text" placeholder="" value=${arrItem.payee_name} id="payee_name">
+        </div>
       </td>
-      <td>${arrItem.credit}</td>
-      <td>${arrItem.debit}</td>
-      <td>
-      <button class="btn btn-primary" type="submit">Approve</button>
+      <td style="padding:10px"; style="margin:0px">
+        <div class="ui input">
+          <input type="text" placeholder="" id="category">
+        </div>
+      </td>
+      <td style="padding:10px"; style="margin:0px">
+        <div class="ui input">
+          <input type="text" placeholder="memo" id="memo">
+        </div>
+      </td>
+      <td style="padding:10px"; style="margin:0px">
+        <div class="ui input">
+          <label for="amount" class="ui label">$</label>
+          <input style="width:90px"; type="text" placeholder="outflow" id="outflow">
+        </div>
+      </td>
+      <td style="padding:10px"; style="margin:0px">
+        <div class="ui input">
+          <label for="amount" class="ui label">$</label>
+          <input style="width:90px"; type="text" placeholder="inflow" id="inflow">
+        </div>
+      </td>
+      <td style="padding: 0px"; style="margin:0px" class="collapsing">
+        <div class="ui fitted toggle checkbox">
+          <input type="checkbox"> <label></label>
+        </div>
       </td>
     </tr>
     `);
+  
+    console.log(arrItem.debit !== "");
+    console.log(arrItem.debit)
+    
+    if (arrItem.credit !== "") {
+      var i = '#ynabTrans' + arrItem.id;
+      $(i).find('#outflow').attr("value", arrItem.credit);
+    } else if (arrItem.debit !== "") {
+      console.log('inside else')
+      var i = '#ynabTrans' + arrItem.id;
+      $(i).find('#inflow').attr("value", arrItem.debit);
+    }
+
   });
 }
+
 function main() {
   var transactionsTable  = document.getElementById('transactionsTable').getElementsByTagName('tr');
 
   var transactions = [];
   // 3rd element begins the row and last element is total
-  for (var i = 3, len = transactionsTable.length - 1; i < len; i++) {
-    transactions.push(treadRowData(transactionsTable[i]));
+  var isPosted = false;
+  for (var i = 2, len = transactionsTable.length - 1; i < len; i++) {
+    
+    if (isPosted) {
+      transactions.push(treadRowData(i-3, transactionsTable[i]));
+    } else if (transactionsTable[i].textContent.trim().split('\n')[0] == "Posted Transactions") {
+      isPosted = true;
+      i++;
+    }
   }
 
-  initBootstrap();
+  initSemanticUI();
   var ynabTable = insertTable();
   fillTable(ynabTable, transactions);
 }
